099b437f64c00ec6715eebe11fc0fc871d589e07
SriramPavu <srirampavu11@gmail.com>
2025-04-24 12:58:45 +0200

    Add files via upload

---
+++ SingularProductAPI/Controller/ProductSalesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SingularProductsAPI.Models;
+using SingularProductsAPI.Services;
+
+namespace SingularProductsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductSalesController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public ProductSalesController(ApplicationDBContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+        [HttpGet("details")]
+        public ActionResult<IEnumerable<Product>> GetProductsDetails()
+        {
+            var products = _dbContext.Products.OrderByDescending(c => c.Id).ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("sales")]
+        public ActionResult<IEnumerable<ProductSales>> GetProductssales()
+        {
+            var products = _dbContext.productSales.OrderByDescending(c => c.SaleId).ToList();
+            return Ok(products);
+        }
+    }
+}
