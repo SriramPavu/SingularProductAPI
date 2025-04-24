a173542595101ce01cc01dbc113dbd37acbf05e7
SriramPavu <srirampavu11@gmail.com>
2025-04-24 13:00:07 +0200

    Add files via upload

---
+++ SingularProductAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingularProductsAPI.Models
+{
+    public class Product
+    {
+        [MaxLength(100)]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        [Required]
+        public string SalePrice { get; set; } = "";
+        [MaxLength(100)]
+        [Required]
+        public string Category { get; set; } = "";
+        [MaxLength(100)]
+        [Required]
+        public string Image { get; set; } = "";
+    }
+}
---
+++ SingularProductAPI/Models/ProductSales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SingularProductsAPI.Models
+{
+    public class ProductSales
+    {
+        [Key]
+        public int SaleId { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string ProductId { get; set; } = "";
+
+        [Required]
+        public decimal SalePrice { get; set; } 
+
+        [Required]
+        public decimal SaleQty { get; set; }
+
+        [Required]
+        public DateTime SaleDate { get; set; } 
+    }
+}
