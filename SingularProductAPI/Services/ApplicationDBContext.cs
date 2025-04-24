01c7997afcf0f2d9f478e23e0a95a1525d5e59c6
SriramPavu <srirampavu11@gmail.com>
2025-04-24 13:09:26 +0200

    Add files via upload

---
+++ SingularProductAPI/Services/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SingularProductsAPI.Models;
+
+namespace SingularProductsAPI.Services
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductSales> productSales { get; set; }
+    }
+}
